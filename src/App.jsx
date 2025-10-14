8d5cb87801272558727ef632d3e45ca1e4657e69
Yadongofficial <35225627+YadongOfficial@users.noreply.github.com>
2025-10-15 06:32:51 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PromptJSONGenerator from './components/PromptJSONGenerator'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PromptJSONGenerator />
+    </div>
+  )
+}
